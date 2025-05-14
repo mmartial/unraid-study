4bad39494ffa8e378f0d6911b6deb18320ce20a9
Martial Michel <7586284+mmartial@users.noreply.github.com>
2025-05-14 15:01:39 -0400

    Only delete the master nginx process

--- varlog_nginx.sh
+++ varlog_nginx.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 
-echo "Cleaning up /var/log/syslog then restarting the syslog daemon"
+echo "Cleaning up /var/log/syslog"
 > /var/log/syslog   &\
 > /var/log/syslog.1 &\
 > /var/log/syslog.2
-/etc/rc.d/rc.syslog stop
-/etc/rc.d/rc.syslog start
 
 
-echo "Cleaning up /var/log/nginx/error.log then restarting the nginx daemon"
-pkill -9 nginx
+echo "Cleaning up /var/log/nginx/error.log"
 > /var/log/nginx/error.log &
 > /var/log/nginx/error.log.1
-/etc/rc.d/rc.nginx start
+
+echo "Find the master process and kill it"
+ps -axfo pid,ppid,uname,cmd | grep nginx | grep -v '\\_' | awk '{print $1}' | xargs kill -9
+
+echo "Wait 10 seconds"
+sleep 10
+
+while true; do
+    echo "Attempting to start nginx"
+    /etc/rc.d/rc.nginx start
+    sleep 10
+    echo "Check nginx status"
+    /etc/rc.d/rc.nginx status | grep "is running" && echo "nginx is running"; exit 0
+done
